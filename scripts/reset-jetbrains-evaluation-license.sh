aa76606bc89cc3e9115c36ec3f710a62e40859c7
Fernando "ferox" dos Santos <ferox@disroot.org>
2022-05-29 15:18:12 -0300

    Adicionando o script que remove de outras IDEs
    Esse script remove as licenças das IDEs: WebStorm e DataGrip

---
+++ scripts/reset-jetbrains-evaluation-license.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Script que faz o reset do evalutaion license do PhpStorm
+JETBRAINS_CONFIG_DIR="$HOME/.config/JetBrains/"
+if [ -d "$JETBRAINS_CONFIG_DIR" ]; then
+	echo 'Removendo a licença trial...'
+	sleep 3
+	echo '###############################################################################'
+	echo '# Digite uma das opções abaixo:                                               #'
+	echo '# ps - Para remover a licença do PhpStorm                                     #'
+	echo '# ws - Para remover a licença do WebStorm                                     #'
+	echo '# dg - Para remover a licença do DataGrip                                     #'
+	echo '# sair - Para sair das opções e a removoção padrão do PhpStorm será executada #'
+	echo '##############################################################################'
+	LAST_VERSION_CONFIG=$(find "$HOME/.config/JetBrains/" -maxdepth 1 -type d -name "PhpStorm2021.2*" | sort -V | tail -1)
+	while :
+	do
+        read INPUT_STRING
+        case $INPUT_STRING in
+            ps)
+                LAST_VERSION_CONFIG=$(find "$HOME/.config/JetBrains/" -maxdepth 1 -type d -name "PhpStorm2021.2*" | sort -V | tail -1)
+                break
+                ;;
+            ws)
+                LAST_VERSION_CONFIG=$(find "$HOME/.config/JetBrains/" -maxdepth 1 -type d -name "WebStorm2021.2*" | sort -V | tail -1)
+                break
+                ;;
+            dg)
+                LAST_VERSION_CONFIG=$(find "$HOME/.config/JetBrains/" -maxdepth 1 -type d -name "DataGrip2021.2*" | sort -V | tail -1)
+                break
+                ;;
+            sair)
+                echo "Removendo a licença trail do PhpStorm..."
+                sleep 3
+                break
+                ;;
+            *)
+                echo "Esta opção não existe!"
+                ;;
+        esac
+    done
+	JETBRAINS_CONSENTOPTIONS=$(find "$HOME/.local/share/JetBrains/" -maxdepth 1 -type d -name "consentOptions*" | sort -V | tail -1)
+	JAVA_USERPREFS=$(find "$HOME/.java/" -maxdepth 1 -type d -name ".userPrefs*" | sort -V | tail -1)
+	echo "#1 Removendo -> $LAST_VERSION_CONFIG/eval"
+	rm -rf "$LAST_VERSION_CONFIG/eval"
+	sleep 2
+	echo "#2 Removendo -> $LAST_VERSION_CONFIG/options/other.xml"
+	rm -rf "$LAST_VERSION_CONFIG/options/other.xml"
+	sleep 2
+	echo "#3 Removendo -> $JETBRAINS_CONSENTOPTIONS"
+	rm -rf "$JETBRAINS_CONSENTOPTIONS"
+	sleep 2
+	echo "#4 Removendo -> $JAVA_USERPREFS"
+	rm -rf "$JAVA_USERPREFS"
+	sleep 2
+	echo "."
+	sleep 1
+	echo ".."
+	sleep 1
+	echo "..."
+	sleep 2
+	echo "Remoção efetuada com sucesso! HAPPY HACKING..."
+	
+else
+	echo 'Nenhuma IDE da JetBrains está instalada na máquina...'
+fi
